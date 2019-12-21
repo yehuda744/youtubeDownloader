d44bc71410fded16dd767744a9f19a67cea9d303
yehuda744 <yehuda.se29@gmail.com>
2019-12-21 21:20:43 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ytdl  = require ('ytdl-core');
+
+var app = express();
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public' ));
+app.get('/', (req, res) => {
+   res.render('index')
+})
+
+// step one : when someone goes here make a request to instagram to send back the information from the user that we want
+// step two: parse that information and store the profile picture in a variable
+// step three: res.send that profile picture
+app.get('/search', (req, res) => {
+    // makes a new variable from the input and u write .search because the object we sent from main was called search and it had the input in it
+    var search = req.query.URL;
+    console.log(search);
+
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+
+    ytdl(search, {
+        format: 'mp4'
+    }).pipe(res);
+});
+
+
+app.listen(process.env.PORT || 8000)
---
+++ public/main.js
@@ -0,0 +1,21 @@
+
+$(document).on('submit', '#form', function(e){
+    var input = document.getElementById('input').value;
+    var t = Date.now();
+    Submit(JSON.stringify(input) , t);
+    console.log(input);
+});
+// thsi si the function that seds what we search for and gets a response then it return all the data from the profile we searched
+// then it parses through and makes an image from the profile pic and also puts the last caption into a var
+function Submit(input, t) {
+    $.ajax({
+        type:'GET',
+        url:`search?URL=${input}`,
+        processData: false,
+        async: true,
+        cache: false,
+        data : false,
+        success: function(response){
+        }
+    });
+}
